505416a20ce77818060d1a4d9e1d689c6d882f60
junedhussain294-afk <junedhussain294@gmail.com>
2026-08-12 16:16:16 +0000

    DAY_11

---
+++ DAY_11/STD.py
@@ -0,0 +1,4 @@
+import numpy as np
+data = np.array([10, 20, 30, 40, 50])
+std_value = np.std(data)
+print("Standard Deviation:", std_value)
---
+++ DAY_11/random_num.py
@@ -0,0 +1,8 @@
+import numpy as np
+rand_int = np.random.randint(1, 101)   # Random integer between 1 and 100
+rand_float = np.random.rand()          # Random float between 0 and 1
+rand_array = np.random.rand(5)         # Random array of size 5
+
+print("Random Integer:", rand_int)
+print("Random Float:", rand_float)
+print("Random Array:", rand_array)
