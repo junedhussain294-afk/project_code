589b516d46f8f2fefecbb0e667cb7ac65eba5036
junedhussain294-afk <junedhussain294@gmail.com>
2026-08-12 16:01:32 +0000

    DAY_9

---
+++ DAY_9/large_num.py
@@ -0,0 +1,9 @@
+def largest_of_three(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+
+print("Largest:", largest_of_three(10, 25, 15))
